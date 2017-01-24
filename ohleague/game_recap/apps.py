80d834bc9f3e24a97cd371bdab6180ee9a4459de
maxwell32 <mpsw30@gmail.com>
2017-01-23 23:10:35 -0500

    created record_game and game_recap apps

---
+++ ohleague/game_recap/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class GameRecapConfig(AppConfig):
+    name = 'game_recap'
--- ohleague/ohleague/settings.py
+++ ohleague/ohleague/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'game_recap.apps.GameRecapConfig',
+    'record_game.apps.RecordGameConfig',
 ]
 
 MIDDLEWARE = [
