23ee33f7478be8cae7790945425c8b9fbb94d90b
maxwell32 <maxwell@Maxs-MBP.local.tld>
2017-02-05 17:24:24 -0500

    VimCrypt~01!ÏØʭ%CzfV7ò[³Òáy²t­Êá^Áÿ³z+¨ö(ë (A0cexëî¥49B3

--- ohleague/ohleague/settings.py
+++ ohleague/ohleague/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
@@ -123,4 +123,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
