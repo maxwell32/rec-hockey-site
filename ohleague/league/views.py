ff25f0cca0fa47656937976ff39417ce8d5f2fdb
maxwell32 <mpsw30@gmail.com>
2017-02-06 22:19:23 -0500

    Changed sql to be able to filter by league_name (ex. E, D, F, Beginner, ect.)

--- ohleague/league/views.py
+++ ohleague/league/views.py
@@ -9,12 +9,14 @@ from team.models import Team
 # Create your views here.
 def index(request):
     #E = 1, hmmm
+    league_name = 'E'
     league_letter = 1
     #goal_list = Player.objects.order_by('g')
-    goal_list = Player.objects.raw('SELECT * FROM player_player AS p JOIN team_team AS t ON p.team_id=t.id WHERE t.id=%s', [league_letter])
-    assist_list = Player.objects.order_by('a')
+    goal_list = Player.objects.raw('SELECT * FROM player_player AS p JOIN team_team AS t ON p.team_id=t.id JOIN league_league AS l ON t.league_id=l.id WHERE l.league_name=%s ORDER BY p.g DESC', [league_name])
+    assist_list = Player.objects.raw('SELECT * FROM player_player AS p JOIN team_team AS t ON p.team_id=t.id WHERE t.id=%s ORDER BY p.a DESC', [league_letter])
 
     context = {
         'goal_list': goal_list,
+        'assist_list': assist_list,
     }
     return render(request, 'league/index.html', context)
